8bfc2e51c1d781263749f8a7201ad5d9133c1088
joseantonino <joseantonino78@gmail.com>
2024-10-06 12:25:14 -0300

    exe:treino

---
+++ exercicios/teste/javaScript.js
@@ -0,0 +1,23 @@
+function eventodblClick() {
+  //document.body.style.backgroundColor = "yellow";
+
+  alert("Evento de clique duplo");
+}
+
+function viraVermelho() {
+  let div = document.getElementById("teste");
+  div.style.backgroundColor = "red";
+}
+
+function viraAzul() {
+  let div = document.getElementById("teste");
+  div.style.backgroundColor = "blue";
+}
+
+function mudou() {
+  console.log("Mudou");
+}
+
+function eventoClick() {
+  document.body.style.backgroundColor = "yellow";
+}
